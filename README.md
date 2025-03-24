f6e73bdcd07ad13f89a5d0a3d91fb8306eb715dd
Nicolas Sampietro <nsampietro@Annas-MacBook-Air.local>
2025-03-24 14:12:41 -0500

    reorg

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 [DearPyGui](https://github.com/hoffstadt/DearPyGui) is an amazing library and yet, I often find myself wishing there were an easy way to define reusable pieces of UI. From React JS documentation:  "A component is a piece of the UI (user interface) that has its own logic and appearance. A component can be as small as a button, or as large as an entire page." Components are a powerful abstraction.   
 
-
 The goal of this project is to test a few ideas on how to bring Components to DearPyGui.  
 
  - Components should be first class citizens, i.e. use the same API as regular Widgets.
@@ -14,4 +13,3 @@ Examples under demo.py:
 
 - Date Picker
 - Data Grid
-
--- src/dpg-components/demo.py
+++ src/dpg-components/demo.py
@@ -1,36 +1,35 @@
 from datetime import datetime
 import pandas as pd
-import dpg_components as dpgx
+import dearpygui.dearpygui as dpg
+import dpg_components
 
-dpgx.create_context()
-dpgx.create_viewport()
-dpgx.setup_dearpygui()
-
+dpg.create_context()
+dpg.create_viewport()
+dpg.setup_dearpygui()
 
 def _on_demo_close(sender, app_data, user_data):
-    
-    dpgx.delete_item('date_picker_1')
-    dpgx.delete_item('text_box_1')
-    dpgx.delete_item(sender)
+    dpg.delete_item('date_picker_1')
+    dpg.delete_item('text_box_1')
+    dpg.delete_item(sender)
 
-    
-with dpgx.window(label="Example Window", on_close=_on_demo_close, width=800, 
+
+with dpg.window(label="Example Window", on_close=_on_demo_close, width=800, 
                  height=800, pos=(100,100) ) as w:
     
-    with dpgx.tree_node(label="Date picker"):
+    with dpg.tree_node(label="Date picker"):
 
-        dpgx.add_component('components','DatePickerComp',tag=f'date_picker_1')
+        dpg.add_component('dpg_components','DatePickerComp',tag=f'date_picker_1')
 
         # Set default date to today
-        dpgx.configure_item(f'date_picker_1', default_value = datetime.now().date() )
+        dpg.configure_item(f'date_picker_1', default_value = datetime.now().date())
     
         # Add another component with the same source as Date picker
-        dpgx.add_component('components','TextBoxComp',tag='text_box_1', source='date_picker_1')
+        dpg.add_component('dpg_components','TextBoxComp',tag='text_box_1', source='date_picker_1')
     
-    with dpgx.tree_node(label="Data Grid"):
+    with dpg.tree_node(label="Data Grid"):
 
         # Add Data Grid component
-        dpgx.add_component('components','DataGridComp',tag=f'data_grid_1')
+        dpg.add_component('dpg_components','DataGridComp',tag=f'data_grid_1')
         
         data = {
             'Name': ['Alice', 'Bob', 'Charlie'],
@@ -40,14 +39,13 @@ with dpgx.window(label="Example Window", on_close=_on_demo_close, width=800,
         df = pd.DataFrame(data)
         
         # Set data grid value
-        dpgx.set_value('data_grid_1', value = df)
-
+        dpg.set_value('data_grid_1', value = df)
+        
         # get component config
-        config = dpgx.get_item_configuration('data_grid_1')
-        print(config)
+        config = dpg.get_item_configuration('data_grid_1')
 
-dpgx.show_item_registry()
+dpg.show_item_registry()
 
-dpgx.show_viewport()
-dpgx.start_dearpygui()
-dpgx.destroy_context()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
